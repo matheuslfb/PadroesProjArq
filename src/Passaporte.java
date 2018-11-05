ff4b77b141c1c36f5e83834db668d0a0810e362c
matheuslfb <matheus.ferreira.001@acad.pucrs.br>
2018-11-05 20:05:32 -0200

    first commit

---
+++ src/Passaporte.java
@@ -0,0 +1,10 @@
+import java.util.GregorianCalendar;
+
+public interface Passaporte {
+    String getNomeCliente();
+    int getNroDias();
+    double valorBasico();
+    double valorDoDia(int nrodia);
+    double valorTotal();
+    GregorianCalendar dataInicial();
+}
---
+++ src/PassaporteBasico.java
@@ -0,0 +1,47 @@
+import java.util.GregorianCalendar;
+
+public class PassaporteBasico implements Passaporte {
+	private String nomeCliente;
+	private int nroDias;
+	private double valorBasico;
+	private GregorianCalendar dataInicial;
+
+	public PassaporteBasico(String umCliente, int umNroDias, double umValorBasico, int dia, int mes, int ano) {
+		nomeCliente = umCliente;
+		nroDias = umNroDias;
+		valorBasico = umValorBasico;
+		dataInicial = new GregorianCalendar(ano, mes, dia);
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public int getNroDias() {
+		return nroDias;
+	}
+
+	public double valorBasico() {
+		return valorBasico;
+	}
+
+	public GregorianCalendar dataInicial() {
+		return (GregorianCalendar) dataInicial.clone();
+	}
+
+	public double valorDoDia(int nrodia) {
+		double valorDia = valorBasico();
+		for (int i = 0; i < nrodia - 1; i++) {
+			valorDia = valorDia - (valorDia * 0.2);
+		}
+		return valorDia;
+	}
+
+	public double valorTotal() {
+		double valorTotal = 0.0;
+		for (int d = 1; d <= getNroDias(); d++) {
+			valorTotal += valorDoDia(d);
+		}
+		return valorTotal;
+	}
+}
